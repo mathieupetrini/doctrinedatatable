4b38c3e2d21dddfd8268d0694daa23aad6185d3b
mpetrini <mathieupetrini@gmail.com>
2018-12-29 13:19:20 +0100

    add wget to image from php7.2

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script origin : https://docs.gitlab.com/ee/ci/examples/php.html
+
 # We need to install dependencies only for Docker
 [[ ! -e /.dockerenv ]] && exit 0
 
@@ -8,6 +10,7 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
+apt-get install wget -yqq
 
 # Install mysql driver
 # Here you can install any other extension that you need

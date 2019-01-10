eb2642f9ee01f2d904198072b7e1218639d8774a
mpetrini <mathieupetrini@gmail.com>
2019-01-10 12:22:37 +0100

    Fix #6

---
+++ src/DoctrineDatatable/ClientSideDatatable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DoctrineDatatable;
+
+/**
+ * Class ServerSideDatatable.
+ *
+ * @author Mathieu Petrini <mathieupetrini@gmail.com>
+ */
+class ClientSideDatatable extends Datatable
+{
+    /**
+     * PUBLIC METHODS.
+     */
+
+    /**
+     * @author Mathieu Petrini <mathieupetrini@gmail.com>
+     *
+     * @param array $filters
+     *
+     * @return array
+     *
+     * @throws Exception\ResolveColumnNotHandle
+     * @throws Exception\UnfilterableColumn
+     * @throws Exception\WhereColumnNotHandle
+     */
+    public function get(array $filters): array
+    {
+        $query = $this->createQueryResult();
+        $this->createFoundationQuery($query, $filters);
+
+        $data = $this->result(
+            $query,
+            isset($filters['order']) ?
+                $filters['order'][0]['column'] :
+                0,
+            isset($filters['order']) ?
+                $filters['order'][0]['dir'] :
+                'ASC'
+        );
+
+        return array(
+            'data' => $data,
+        );
+    }
+}
